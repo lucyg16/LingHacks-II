106ffad0b7f2dd9f9cc7f92a295685013eb558e6
Lucy <lucyguo8@gmail.com>
2019-03-30 15:00:14 -0700

    import statements

--- script.py
+++ script.py
@@ -1,4 +1,17 @@
+import random
+import csv
 import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC 
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+from collections import Counter
+nltk.download("stopwords")
+nltk.download("wordnet")
 
 from nltk.corpus import wordnet as wn
 
