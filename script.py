6eae836a53870e5e3fcf22e2f90de2706b4da022
ElenaAtluri <elena@atluri.co>
2019-03-30 11:45:39 -0700

    pseudocode python

---
+++ script.py
@@ -0,0 +1,16 @@
+import nltk
+
+from nltk.corpus import wordnet as wn
+
+# take out keywords from a user inputted text
+
+# for each mental disorder, create a 'similarity score' based on the keywords
+# calculate similarity between keyword against each symptom of mental disorder
+# calculate overall similarity for mental disorder based on how many symptoms matched
+# for each mental disorder:
+#   for each keyword:
+#       for each symptom:
+#           how similar?
+# calculate similarity score
+
+# return mental disorder with the highest 'similarity score'
