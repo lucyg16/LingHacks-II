c4dc7c6792500913d8ff57f3a16b21f2f97288ec
Lucy <lucyguo8@gmail.com>
2019-03-30 18:10:00 -0700

    remove all stopwords

--- parse.py
+++ parse.py
@@ -13,18 +13,30 @@ print("\n\nIs this how you are feeling?:\n",user_paragraph)
 
 #turning input into smaller strings
 all_words = user_paragraph.split()
-items = len(all_words)
-print(items)
 
 #cleaning the input
 stop_words = stopwords.words("english")
 exclude = set(string.punctuation)
 
-for i in (0,items-1):
+i=0
+while (i < len(all_words)):
 	print(i)
 	if all_words[i] in stop_words:
 		all_words.remove(all_words[i])
-	i=i+1
+	else:
+		i=i+1
+
+
+# text = [point[1] for point in all_words]
+# stop_words = stopwords.words("english")
+# exclude = set(string.punctuation)
+
+# def clean(doc):
+# 	stop_free = " ".join([i for i in doc.lower().split() if i not in stop_words])
+# 	punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+
+# text = np.array([clean(point) for point in text])
+
 
 
 
